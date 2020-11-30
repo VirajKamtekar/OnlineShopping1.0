697ffb5a863f8e060a07fe9dfc0ec3baa8887151
VirajKamekar <viraj.kamtekar@gmail.com>
2020-12-01 00:23:58 +0530

    HomepageComponentCreated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home-comp/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
